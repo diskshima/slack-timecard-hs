d83a636860bb289992c207c7d6f18b4e4135aaa5
Daisuke Shimamoto <diskshima@gmail.com>
2017-08-20 17:50:12 +0900

    Add command echoing

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import Data.Time.Clock
-import Data.Time.LocalTime
-import Data.Time.Format
-import Text.Printf
+import Control.Monad.Except (runExceptT)
+
+import Data.Text
+import Data.Time.Clock (UTCTime, getCurrentTime)
+import Data.Time.LocalTime (TimeZone, getCurrentTimeZone, utcToZonedTime)
+import Data.Time.Format (formatTime, defaultTimeLocale)
+import Text.Printf (printf)
+
+import Network.Wai.Handler.Warp (run)
+import Network.Linklater
 
 convertTime :: UTCTime -> TimeZone -> String
 convertTime time tz =
@@ -29,10 +37,16 @@ sendCommand command timeStr =
     "finish" -> processFinish timeStr
     _ -> putStrLn $ printf "Do not know how to process command: %s" command
 
+timestamp :: Command -> IO Text
+timestamp (Command command user_ channel (Just query)) = do
+  putStrLn $ printf "Command: %s" (show command)
+  let config = Config $ "SLACK_URL"
+  let message = SimpleMessage (EmojiIcon $ "gift") "Bot" channel "start"
+  runExceptT $ say message config
+  return "ok"
 
 main :: IO ()
 main = do
-  prompt
-  command <- getLine
-  timeStr <- getCurrentTimeString
-  sendCommand command timeStr
+  putStrLn $ printf "Listening on port %d" port
+  run port (slashSimple timestamp)
+    where port = 8833
